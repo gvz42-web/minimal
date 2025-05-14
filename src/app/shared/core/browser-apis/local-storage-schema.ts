22bac3bcb6ce628ee3e0f11c6ea9ea56d8262d31
GVZ42 <gvz42.web@gmail.com>
2025-05-14 18:51:00 +0700

    feat(save theme to localStorage and update theme CSS class with renderer):

--- src/app/shared/core/browser-apis/local-storage-schema.ts
+++ src/app/shared/core/browser-apis/local-storage-schema.ts
@@ -1,6 +1,8 @@
 import { Language } from '../i18n/languages';
+import { Theme } from '../theme/themes';
 
 export type LocalStorageSchema = {
   token: string;
   lang: Language;
+  theme: Theme;
 };
--- src/app/shared/core/theme/theme-service.ts
+++ src/app/shared/core/theme/theme-service.ts
@@ -1,16 +1,35 @@
 import { DOCUMENT } from '@angular/common';
-import { effect, inject, Injectable, signal } from '@angular/core';
-import { defaultTheme, Theme } from './themes';
+import {
+  effect,
+  inject,
+  Injectable,
+  RendererFactory2,
+  signal,
+} from '@angular/core';
+import { LocalStorage } from '../browser-apis/local-storage';
+import { defaultTheme, Theme, themes } from './themes';
+
+const themeClasses = themes.map(theme => 'theme-' + theme);
 
 @Injectable({ providedIn: 'root' })
 export class ThemeService {
   private document = inject(DOCUMENT);
+  private localStorage = inject(LocalStorage);
+  private renderer = inject(RendererFactory2).createRenderer(null, null);
 
-  private theme = signal<Theme>(defaultTheme);
+  private theme = signal<Theme>(this.localStorage.get('theme') || defaultTheme);
   currentTheme = this.theme.asReadonly();
 
   private themeEffect = effect(() => {
-    this.document.documentElement.className = 'theme-' + this.theme();
+    this.localStorage.set('theme', this.currentTheme());
+
+    for (const themeClass of themeClasses) {
+      this.renderer.removeClass(this.document.documentElement, themeClass);
+    }
+    this.renderer.addClass(
+      this.document.documentElement,
+      'theme-' + this.currentTheme()
+    );
   });
 
   setTheme(theme: Theme) {
